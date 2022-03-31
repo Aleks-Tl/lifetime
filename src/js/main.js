6de11dd7efd404fb32a0f4725a1e3acdd3e3912a
Aleksandr Tloka <aleksandr.tloka@i.ua>
2022-03-31 21:34:11 +0300

    finished home page

--- src/js/main.js
+++ src/js/main.js
@@ -31,4 +31,39 @@ document.addEventListener("DOMContentLoaded", function (event) {
   scrollFunction();
 });
 
+// Swiper
+
+const swiper = new Swiper('.slider-testimonials', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      centeredSlides: true
+    },
+    // when window width is >= 480px
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      centeredSlides: true
+    },
+    // when window width is >= 640px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    }
+  }
+
+
+});
+
 
